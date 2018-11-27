5916aa2cfe804dd0b5d8f32ed72741dac9794160
mykola-h <31933152+mykola-h@users.noreply.github.com>
2018-11-27 11:42:07 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,25 +2,30 @@ import sys
 
 def arithmetic():
     not_number = 'The given value "{}" is not a number! Try again.'
-    operations_list = ('+', '-', '/')
     a = input('Please, enter the first number...') 
     try:
         first_number = int(a) 
     except ValueError: 
+        first_number = float(a) 
+    except ValueError:
         print(not_number.format(a))
-        sys.exit(0) 
+        sys.exit(0)
     b = input('Please, enter the second number...') 
     try:
         second_number = int(b)
     except ValueError: 
+        second_number = float(b)
+    except ValueError: 
         print(not_number.format(b))
         sys.exit(0)
-    c = input('What opeartion should we do? You can choose between +, - or /...')
-    if (c is not in operations_list):
+    c = input('What opeartion should we do? You can choose between +, -, * or /...')
+    operations = {'+': (first_number + second_number), '-': (first_number - second_number),
+                  '*': (first_number * second_number), '/': (first_number / second_number)}
+    if (c not in operations):
         print('The given operation is not recognized! Try again.')
         sys.exit(0)
     else:
-        result = a + c + b
+        result = operations[c]
         print('The result is: {}'.format(result))
 
 arithmetic()
