5af5e4c98588dd779d26a49d3fd2eab1b6eb154e
mykola-h <31933152+mykola-h@users.noreply.github.com>
2018-12-03 09:32:58 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,31 +1,34 @@
-import sys
-
-def arithmetic():
-    not_number = 'The given value "{}" is not a number! Try again.'
+import sys 
+ 
+print('This function performs simple arithmetic operations with the given two numbers.')
+ 
+def arithmetic(): 
+    not_number = 'The given value "{}" is not a number! Try again.' 
     a = input('Please, enter the first number...') 
-    try:
+    try: 
         first_number = int(a) 
     except ValueError: 
-        first_number = float(a) 
-    except ValueError:
-        print(not_number.format(a))
-        sys.exit(0)
+        try: 
+            first_number = float(a) 
+        except ValueError: 
+            print(not_number.format(a)) 
+            sys.exit(0) 
     b = input('Please, enter the second number...') 
-    try:
-        second_number = int(b)
+    try: 
+        second_number = int(b) 
     except ValueError: 
-        second_number = float(b)
-    except ValueError: 
-        print(not_number.format(b))
-        sys.exit(0)
-    c = input('What opeartion should we do? You can choose between +, -, * or /...')
-    operations = {'+': (first_number + second_number), '-': (first_number - second_number),
-                  '*': (first_number * second_number), '/': (first_number / second_number)}
-    if (c not in operations):
-        print('The given operation is not recognized! Try again.')
-        sys.exit(0)
-    else:
-        result = operations[c]
-        print('The result is: {}'.format(result))
-
-arithmetic()
+        try: 
+            second_number = float(b) 
+        except ValueError: 
+            print(not_number.format(b)) 
+            sys.exit(0) 
+    c = input('What opeartion should we do? You can choose between +, - or /...') 
+    operations = {'+': (first_number + second_number), '-': (first_number - second_number), '*': (first_number * second_number), '/': (first_number / second_number)} 
+    if (c not in operations): 
+        print('The given operation is not recognized! Try again.') 
+        sys.exit(0) 
+    else: 
+        result = operations[c] 
+        print('The result is: {}'.format(result)) 
+ 
+arithmetic() 
