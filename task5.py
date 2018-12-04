18d77fad85787c7deed14f9e0ce2c5525ebdca17
mykola-h <31933152+mykola-h@users.noreply.github.com>
2018-12-04 22:11:48 +0200

    Create task5.py

---
+++ task5.py
@@ -0,0 +1,34 @@
+import sys 
+ 
+print('This function calculates amount of money on the bank deposit account after its time has passed') 
+ 
+def bank(): 
+    a = input('Please, enter amount of money for the deposit account...') 
+    try: 
+        amount = int(a) 
+    except ValueError: 
+        try: 
+            amount = float(a) 
+        except ValueError: 
+            print('The given value is not a number! Try again.') 
+            sys.exit(0) 
+    if (amount <= 0): 
+        print('Amount cannot be equal 0 or a negative value! Try again.') 
+    else: 
+        y = input('Please, enter period for the deposit account in years...') 
+        try: 
+            years = int(y) 
+        except ValueError: 
+            try: 
+                years = float(y) 
+            except ValueError: 
+                print('The given value is not a number! Try again.') 
+                sys.exit(0) 
+    if (years <= 0): 
+        print('Period cannot be equal 0 or a negative value! Try again.') 
+    else: 
+        for i in years: # it's not working! 
+            amount = amount * 1.2 
+        print('After {} years you will have {} on your deposit account.'.format(years, amount)) 
+ 
+bank() 
