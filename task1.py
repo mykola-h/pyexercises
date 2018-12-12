32d9f7e0da7031fe25dea29999df4f8db4c33f95
mykola-h <31933152+mykola-h@users.noreply.github.com>
2018-12-12 21:56:42 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -30,7 +30,6 @@ def arithmetic():
     c = input('What opeartion should we do? You can choose between +, - or /...') 
     operations = {'+': (first_number + second_number), '-': (first_number - second_number),
                   '*': (first_number * second_number), '/': (first_number / second_number)} 
->>>>>>> 3f80d53f207e33129daa884903e3920268162869
     if (c not in operations): 
         print(color.red + 'The given operation is not recognized! Try again.' + color.end) 
         return 
